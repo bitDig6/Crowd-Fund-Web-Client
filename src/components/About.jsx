58167fe6e668ff68a1863ec6794dc5234c15c258
bitDig6 <ummenabilajalal@gmail.com>
2025-04-28 20:26:09 +0600

    added react lottie animation and added about section on hompage

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Fade, Slide } from "react-awesome-reveal";
+import Lottie from 'lottie-react';
+import lottieAnimation from '../../public/lottie.json';
 
 const About = () => {
     return (
@@ -9,9 +11,18 @@ const About = () => {
             </Slide>
             <h3 className='font-rancho text-5xl text-center font-bold text-pink-600 mb-5'>CrowdCube</h3>
             <div className='text-center text-pink-600 text-xl font-semibold'>
-            <Fade delay={1e3} cascade damping={1e-1}>
-                Your Reliable Ally For Fundraising through Crowdfunding
-            </Fade>
+                <Fade delay={1e3} cascade damping={1e-1}>
+                    Your Reliable Ally For Fundraising through Crowdfunding
+                </Fade>
+            </div>
+            <div className='w-4/5 mx-auto flex gap-5 justify-center items-center p-10'>
+                <div className='w-1/2 space-y-3 text-base font-semibold text-justify'>
+                    <p>CrowdCube is a reliable and popular platform for raising funds through Crowdfunding. We have a 24/7 customer support system and we are always ready to start your crowdfunding campaign.
+                    </p>
+                    <p>Contact Us for any Kind of Queries: <span className='text-pink-600 underline underline-offset-2'>+088 179797977</span></p>
+                    <p>Also, follow us on Social Media to stay updated with us. We are available at FaceBook, X, and Instagram. Search for 'CrowCube' and you can find our pages.</p>
+                </div>
+                <Lottie className='bg-neutral w-1/3 mx-auto outline-2 outline-offset-8 border-2 border-pink-400 outline-pink-400' animationData={lottieAnimation}></Lottie>
             </div>
         </section>
     );
--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Slide } from 'react-awesome-reveal';
 
 const Awards = () => {
     return (
-        <section className='w-11/12 mx-auto'>
-            <h3>Awards</h3>
+        <section className='w-11/12 mx-auto my-20'>
+            <Slide>
+                <h3 className='text-3xl text-center font-bold text-pink-600 mb-5'>Awards</h3>
+            </Slide>
         </section>
     );
 };
