768b9694c98529aa38cf4a8f668d15507f091761
bitDig6 <ummenabilajalal@gmail.com>
2025-04-29 12:57:11 +0600

    added awards section in home page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,19 +10,21 @@ const About = () => {
                 <h3 className='text-3xl text-center font-bold text-pink-600 mb-5'>About Us</h3>
             </Slide>
             <h3 className='font-rancho text-5xl text-center font-bold text-pink-600 mb-5'>CrowdCube</h3>
-            <div className='text-center text-pink-600 text-xl font-semibold'>
+            <div className='font-raleway text-center text-pink-600 text-xl font-bold mb-5'>
                 <Fade delay={1e3} cascade damping={1e-1}>
-                    Your Reliable Ally For Fundraising through Crowdfunding
+                    <div>Your Reliable Ally</div>
+                    <div>For Fundraising</div>
+                    <div>Through Crowdfunding</div>                  
                 </Fade>
             </div>
-            <div className='w-4/5 mx-auto flex gap-5 justify-center items-center p-10'>
-                <div className='w-1/2 space-y-3 text-base font-semibold text-justify'>
+            <div className='w-4/5 mx-auto flex flex-col-reverse lg:flex-row gap-5 justify-center items-center lg:p-10'>
+                <div className='lg:w-1/2 space-y-3 text-base font-semibold text-justify'>
                     <p>CrowdCube is a reliable and popular platform for raising funds through Crowdfunding. We have a 24/7 customer support system and we are always ready to start your crowdfunding campaign.
                     </p>
                     <p>Contact Us for any Kind of Queries: <span className='text-pink-600 underline underline-offset-2'>+088 179797977</span></p>
                     <p>Also, follow us on Social Media to stay updated with us. We are available at FaceBook, X, and Instagram. Search for 'CrowCube' and you can find our pages.</p>
                 </div>
-                <Lottie className='bg-neutral w-1/3 mx-auto outline-2 outline-offset-8 border-2 border-pink-400 outline-pink-400' animationData={lottieAnimation}></Lottie>
+                <Lottie className='lg:w-1/3 mx-auto outline-2 outline-offset-8 border-2 border-pink-400 outline-pink-400' animationData={lottieAnimation}></Lottie>
             </div>
         </section>
     );
--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -1,12 +1,62 @@
 import React from 'react';
 import { Slide } from 'react-awesome-reveal';
+import awardsAnimation from '../../public/awards.json';
+import Lottie from 'lottie-react';
 
 const Awards = () => {
     return (
-        <section className='w-11/12 mx-auto my-20'>
+        <section className='w-11/12 mx-auto my-20 space-y-4'>
             <Slide>
-                <h3 className='text-3xl text-center font-bold text-pink-600 mb-5'>Awards</h3>
+                <h3 className='text-3xl text-center font-bold text-pink-600'>Awards</h3>
             </Slide>
+            <Lottie className='w-1/3 lg:w-1/4 mx-auto' animationData={awardsAnimation}></Lottie>
+            <p className='text-center font-semibold'>CrowdCube is a vastly recognized crowdfunding platform that we have achieved from our community and partners. Checkout some of our achievements here that we deeply cherish</p>
+
+            <div className='w-4/5 mx-auto lg:my-10 grid grid-cols-1 md:grid-cols-3 gap-4'>
+
+                <div className="card bg-gradient-to-br from-pink-400 to-yellow-300 shadow-xl rounded-xl">
+                    <div className="card-body font-poppins">
+                        <h3 className='text-xl font-bold text-center'>Best Crowdfunding Platform 2024</h3>
+                        <p className='text-center font-semibold'>Awarded by the National Innovative Youth Leadership Award 2022</p>
+                    </div>
+                </div>
+
+                <div className="card bg-gradient-to-br from-pink-400 to-yellow-300 shadow-xl rounded-xl">
+                    <div className="card-body font-poppins">
+                        <h3 className='text-xl font-bold text-center'>Excellence In User Experience 2022</h3>
+                        <p className='text-center font-semibold'>Top Listed User Experience Website in Bangladesh IT Today Magazine</p>
+                    </div>
+                </div>
+
+                <div className="card bg-gradient-to-br from-pink-400 to-yellow-300 shadow-xl rounded-xl">
+                    <div className="card-body font-poppins">
+                        <h3 className='text-xl font-bold text-center'>80+ Successful Campaign Milestone 2025</h3>
+                        <p className='text-center font-semibold'>As of 2025, in 10 years in out journey we have successfully completed 80+ campaigns</p>
+                    </div>
+                </div>
+
+
+                <div className="card bg-gradient-to-br from-pink-400 to-yellow-300 shadow-xl rounded-xl">
+                    <div className="card-body font-poppins">
+                        <h3 className='text-xl font-bold text-center'>Top 20 Startup Platforms in 2023</h3>
+                        <p className='text-center font-semibold'>Featured in Youth Innovation BD Magazine as one of he top 20 reliable and impactful platforms for young people for supporting their creative projects</p>
+                    </div>
+                </div>
+
+                <div className="card bg-gradient-to-br from-pink-400 to-yellow-300 shadow-xl rounded-xl">
+                    <div className="card-body font-poppins">
+                        <h3 className='text-xl font-bold text-center'>National Social Welfare Award 2020</h3>
+                        <p className='text-center font-semibold'>Awarded for successful campaigns for donations for various social welfare programs.</p>
+                    </div>
+                </div>
+
+                <div className="card bg-gradient-to-br from-pink-400 to-yellow-300 shadow-xl rounded-xl">
+                    <div className="card-body font-poppins">
+                        <h3 className='text-xl font-bold text-center'>Oricon Cultural Award 2018</h3>
+                        <p className='text-center font-semibold'>Awarded by the Oricon group for holding a handful of campaigns for creative and impactful Documentaries, Indie Films etc.</p>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
